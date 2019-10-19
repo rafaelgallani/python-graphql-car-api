6d1824e2622f7051695257cdcfff732abb4b4417
rafaelgalani <galani.rafael@gmail.com>
2019-10-19 18:55:56 -0300

    adding real fipe values to the script

--- scrapper.py
+++ scrapper.py
@@ -3,43 +3,100 @@ import requests, json, time, os
 interval = .100
 output_dir = 'data'
 debug = True
-base_url = 'http://fipeapi.appspot.com/api/1/carros'
+base_url = 'https://veiculos.fipe.org.br/api/veiculos/'
 
 def output(s):
     if debug:
         print(s)
 
-def get(url):
+def post(url, params):
     
-    result = requests.get(url)
+    if not url.startswith(base_url):
+        url = base_url + url
 
-    while not result.ok:
-        output('Could not query for "{url}". Trying again after {interval:0.0f} milliseconds.'.format(url=url, interval=interval*1000))
-        time.sleep(interval)
-        result = requests.get(url)
-    
-    output('Successfully queried "{url}".'.format(url=url))
-    return json.loads(result.text)
+    if len(params.items()):
+        url += '?'
+        for key, value in params.items():
+            url += '{key}={value}&'.format(key=key, value=value)
+        url = url[:-1]
+
+    request_completed = False
+    exception_counter = 0
+    while not request_completed:
+        try:
+            result = requests.post(url)
+
+            while not result.ok:
+                output('Could not query for "{url}". Trying again after {interval:0.0f} milliseconds.'.format(url=url, interval=interval*1000))
+                time.sleep(interval)
+                result = requests.post(url)
+
+            request_completed = True        
+            output('Successfully queried "{url}".'.format(url=url))
+            return json.loads(result.text)
+        except ConnectionError as e:
+            exception_counter += 1
+            output('=== EXCEPTION ===\n{}: {}'.format(e.errno, e.strerror))
+            request_completed = False
+
+            if exception_counter >= 10:
+                ans = None
+                while ans != 'y' and ans != 'n':
+                    ans = input('More than 10 exceptions have occurred. Do you want to continue? (Y or N)').lower()
+                    if ans == 'y':
+                        exception_counter = 0 
+                    else:
+                        raise Exception('More than 10 exceptions. User stopped the script.')
+
 
 def scrap():
-    makes_result = get('{base_url}/marcas.json'.format(base_url=base_url))
+    makes_result = post('ConsultarMarcas', {
+        'codigoTabelaReferencia':247,
+        'codigoTipoVeiculo': 1,
+    })
 
-    for make in makes_result:
-        make_id = make['id']
-        models = get('{base_url}/veiculos/{make_id}.json'.format(base_url=base_url, make_id=make_id))
-        make['models'] = models
+    try:
+        for make in makes_result:
+            make_id = make['Value']
+            models = post('ConsultarModelos', {
+                'codigoTabelaReferencia':247,
+                'codigoTipoVeiculo': 1,
+                'codigoMarca': make_id,
+            })
+            make['models'] = models['Modelos']
+            
+            for model in make['models']:
+                model_id = model['Value']
+                versions = post('ConsultarAnoModelo', {
+                    'codigoTabelaReferencia':247,
+                    'codigoTipoVeiculo': 1,
+                    'codigoMarca': make_id,
+                    'codigoModelo': model_id,
+                })
 
-        for model in models:
-            model_id = model['id']
-            versions = get('{base_url}/veiculo/{make_id}/{model_id}.json'.format(base_url=base_url, make_id=make_id, model_id=model_id))
+                model['versions'] = versions
 
-            model['versions'] = versions
+                for version in versions:
+                    version_id = version['Value']
+                    version_year, version_fuel_type = version_id.split('-')
+                    
+                    version_details = post('ConsultarValorComTodosParametros', {
+                        'codigoTabelaReferencia':247,
+                        'codigoTipoVeiculo': 1,
+                        'codigoMarca': make_id,
+                        'codigoModelo': model_id,
+                        'anoModelo': version_year,
+                        'codigoTipoCombustivel': version_fuel_type,
+                        'tipoVeiculo': 'carro',
+                        'tipoConsulta': 'tradicional',
+                    })
 
-            for version in versions:
-                version_id = version['id']
-                version_details = get('{base_url}/veiculo/{make_id}/{model_id}/{version_id}.json'.format(base_url=base_url, make_id=make_id, model_id=model_id, version_id=version_id))
-
-                version['details'] = version_details
+                    version['details'] = version_details
+                    #break
+                #break
+            #break
+    except:
+        output('Scrapping stopped by user action.')        
 
     output('Car models successfully retrieved. Outputting file...')
 
